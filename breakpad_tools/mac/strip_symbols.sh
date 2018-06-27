6997177f5b066abcbef73bc1198844a242f6ece0
Rishabh Gupta <rishabh9511@gmail.com>
2018-06-27 15:31:16 +0530

    mac: add breakpad tools for mac

---
+++ breakpad_tools/mac/strip_symbols.sh
@@ -0,0 +1,16 @@
+breakpad_path=$1
+binary_path=$2
+binary_name=$3
+symbol_file="$binary_name.sym"
+
+$breakpad_path/mac/dump_syms $binary_path > $symbol_file
+
+uuid=$(head -n1 $symbol_file | ggrep -o -P "(?<=x86_64).*(?=$binary_name)" | xargs ) 
+
+
+mkdir -p symbols/$binary_name/$uuid/
+
+mv $symbol_file symbols/$binary_name/$uuid/
+
+
+$breakpad_path/mac/sentry-cli --auth-token 00985397724343d496af4d0f88dd8c79adc5f7a5433b4ed9881b9535d0bc5eb4 upload-dif -t breakpad --project el-maven-logging --org test-acc ./symbols/
