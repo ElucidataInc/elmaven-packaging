70e6128d97977531e6a80d76959b408f4edf67e0
Saiful B. Khan <saiful.khan@elucidata.io>
2020-01-17 07:25:12 +0530

    Update configuration files

--- update_version.py
+++ update_version.py
@@ -1,23 +1,18 @@
 import xml.etree.ElementTree as ET
 import sys
-
+from datetime import datetime
 
 tree = ET.parse("config/config.xml")
 root = tree.getroot()
-
 version = sys.argv[1]
-
-
 root.find("Version").text = version
-
 tree.write("config/config.xml")
-
 
 tree = ET.parse("packages/com.vendor.product/meta/package.xml")
 root = tree.getroot()
-
-root.find("DisplayName").text = "El-Maven " +  version
-root.find("Description").text = "Install El-Maven " +  version
+now = datetime.now()
+root.find("ReleaseDate").text = "{}-{:02d}-{:02d}".format(now.year, now.month, now.day)
+root.find("DisplayName").text = "El-MAVEN " +  version
+root.find("Description").text = "Install El-MAVEN " +  version
 root.find("Version").text = version
-
 tree.write("packages/com.vendor.product/meta/package.xml")
